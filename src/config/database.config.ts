e209dbfd3fe60cb3083626d77c6c7318ee808517
Marcos Lopes <marcoslopesg7@gmail.com>
2021-11-22 08:54:45 -0300

    Config Heroku

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,9 @@
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'root',
-    database: 'teste_pr',
-    port: 5433
+    user: 'tjavuqdklerxhc',
+    host: 'ec2-34-206-238-105.compute-1.amazonaws.com',
+    password: '7ae53c04966ae334b7ca84d52282489dc35a143d6e747e89b351328b9d3ee5ff',
+    database: 'de9baobq5npe8n',
+    port: 5432
 });
